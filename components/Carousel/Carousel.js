2e7d47b21e38204ff2b3ba6ec3a3a91350847c35
Joshua Kinslow <killajk2201@gmail.com>
2019-04-20 02:49:51 +0000

    Finished with the project at 9:50 pm CST. started at 8:00 pm CST.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,56 @@
 class Carousel {
+    
+    constructor(carouselElement) {
+        this.carouselElement = carouselElement;
+        
+        this.carouselImages = this.carouselElement.querySelectorAll("div.carousel img");
+        console.log(this.carouselImages);
+        
+        this.currentIndex = 0;
+        this.carouselContainer = this.carouselElement.querySelector(".container");
+        
+        // this.carouselImages[this.currentIndex].style.display = "inline";
+        
+        this.leftBtn = this.carouselElement.querySelector(".left-button");
+        this.rightBtn = this.carouselElement.querySelector(".right-button");
+        
+        this.leftBtn.addEventListener('click', this.prevImage.bind(this));
+        this.rightBtn.addEventListener('click', this.nextImage.bind(this));
+        
+        this.slider_val = 0;
+        this.sliderCount = this.carouselImages.length;
+        this.img_width = 1200;
+
+
+    }
+    
+    updatePosition() {
+        var newLeft = -1 * (this.slider_val * this.img_width);
+        this.carouselContainer.style.left = `${newLeft}px`;
+    }
+    
+    prevImage() {
+        console.log("Previous Carousel Image");
+        this.slider_val--;
+        if (this.slider_val < 0)
+            this.slider_val = this.sliderCount - 1;
+        this.updatePosition();
+    }
+    
+    nextImage() {
+        console.log("Next Carousel Image");
+        this.slider_val++;
+        if (this.slider_val >= this.sliderCount)
+            this.slider_val = 0;
+        this.updatePosition();
+        
+    }
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector("div.carousel");
+carousel = new Carousel(carousel);
+// console.log(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
